c2e53aae9536af060c3eb504de250060c0719d32
web-dev-cohort <rakeshbhuyan0101@gmail.com>
2024-10-20 00:07:25 +0530

    Connect to mongoDb compass

---
+++ src/db/index.js
@@ -0,0 +1,32 @@
+
+// import mongoose, { connection } from "mongoose";
+// import { DB_NAME } from "../constrants.js";
+
+
+// const connectDB = async () => {
+//     try{
+//        const connectionInstance =  await mongoose.connect ( `${process.env.MONGODB_URL}/${DB_NAME}`)
+//        console.log(`MONGODB connected !! DB HOST : ${connectionInstance.connection.host}`)
+//     } catch (error) {
+//         console.log("MONGODB connection error" , error)
+//         process.exit(1)
+//     }
+// }
+
+// export default connectDB
+
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection FAILED ", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB
